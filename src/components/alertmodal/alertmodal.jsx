02b2cb47fe4f68ae4296a386d847f02d5faf32d5
gokulgangadharank <gokulgangadharan79@gmail.com>
2025-11-22 15:25:10 +0530

    error fixing

---
+++ src/components/alertmodal/alertmodal.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const alertmodal = () => {
+  return (
+    <div className='modal'>
+        <div className="modal-header">
+            
+        </div>
+        <div className="modal-actions"></div>
+    </div>
+  )
+}
+
+export default alertmodal
--- src/components/create/create.jsx
+++ src/components/create/create.jsx
@@ -1,7 +1,7 @@
 import Close from '../../assets/images/icons/closeicon.svg'
 import '../create/create.css'
 import Arrow from '../../assets/images/icons/arrowicon.svg'
-import React, { useState } from "react";
+import { useState } from "react";
 import { useDispatch } from "react-redux";
 import { addProject } from "../../reudx/projectslice";
 import { DatePicker } from "antd";
@@ -38,7 +38,7 @@ const Create = ({ onClose }) => {
     );
 
     alert("Project added to Redux!");
-    onClose(); 
+    onClose();
   };
 
   const handleCancel = () => {
@@ -54,130 +54,132 @@ const Create = ({ onClose }) => {
   };
 
   return (
-    <div className='create-container'>
-      <div className="create-tab">
-        <div className="header">
-          <h1>Create Project</h1>
-          <img
-            src={Close}
-            alt="close"
-            className="close-icon"
-            onClick={onClose}
-            style={{ cursor: "pointer" }}
-          />
-        </div>
-
-        <div className="create-project-container">
-
-          <div className="form-group">
-            <label>Project code</label>
-            <input
-              type="text"
-              placeholder="Enter project code"
-              value={projectCode}
-              onChange={(e) => setProjectCode(e.target.value)}
+    <div className="overlay">
+      <div className='create-container'>
+        <div className="create-tab">
+          <div className="header">
+            <h1>Create Project</h1>
+            <img
+              src={Close}
+              alt="close"
+              className="close-icon"
+              onClick={onClose}
+              style={{ cursor: "pointer" }}
             />
           </div>
 
-          <div className="form-group">
-            <label>Project name</label>
-            <input
-              type="text"
-              placeholder="Enter project name"
-              value={projectName}
-              onChange={(e) => setProjectName(e.target.value)}
-            />
-          </div>
+          <div className="create-project-container">
 
-          <div className="form-group">
-            <label>Project status</label>
-            <input
-              type="text"
-              placeholder="Enter project status"
-              value={projectStatus}
-              onChange={(e) => setProjectStatus(e.target.value)}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Project type</label>
-            <div className="select-box">
-              <select
-                value={projectType}
-                onChange={(e) => setProjectType(e.target.value)}
-              >
-                <option>Client</option>
-                <option>Internal Projects</option>
-              </select>
-              <span className="arrow"><img src={Arrow} alt="" /></span>
-            </div>
-          </div>
-
-          <div className="form-group">
-            <label>Project owner</label>
-            <input
-              type="text"
-              placeholder="Enter the project owner"
-              value={projectOwner}
-              onChange={(e) => setProjectOwner(e.target.value)}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Estimated budget</label>
-            <div className="budget-box">
-              <div className="currency">$</div>
+            <div className="form-group">
+              <label>Project code</label>
               <input
-                type="number"
-                placeholder="Enter amount"
-                value={estimatedBudget}
-                onChange={(e) => setEstimatedBudget(e.target.value)}
+                type="text"
+                placeholder="Enter project code"
+                value={projectCode}
+                onChange={(e) => setProjectCode(e.target.value)}
               />
             </div>
-          </div>
 
-          <div className="form-group">
-            <label>Project complexity</label>
-            <input
-              type="text"
-              placeholder="Enter the project complexity"
-              value={projectComplexity}
-              onChange={(e) => setProjectComplexity(e.target.value)}
-            />
-          </div>
-
-          <div className="form-group">
-            <label>Estimated project start date</label>
-            <div className="date-box">
-              <DatePicker
-                format="DD/MM/YYYY"
-                onChange={(date, dateString) => setStartDate(dateString)}
-                className="antd-date"
+            <div className="form-group">
+              <label>Project name</label>
+              <input
+                type="text"
+                placeholder="Enter project name"
+                value={projectName}
+                onChange={(e) => setProjectName(e.target.value)}
               />
             </div>
-          </div>
-          <div className="form-group">
-            <label>Estimated project end date</label>
-            <div className="date-box">
-              <DatePicker
-                format="DD/MM/YYYY"
-                onChange={(date, dateString) => setEndDate(dateString)}
-                className="antd-date"
+
+            <div className="form-group">
+              <label>Project status</label>
+              <input
+                type="text"
+                placeholder="Enter project status"
+                value={projectStatus}
+                onChange={(e) => setProjectStatus(e.target.value)}
               />
+            </div>
+
+            <div className="form-group">
+              <label>Project type</label>
+              <div className="select-box">
+                <select
+                  value={projectType}
+                  onChange={(e) => setProjectType(e.target.value)}
+                >
+                  <option>Client</option>
+                  <option>Internal Projects</option>
+                </select>
+                <span className="arrow"><img src={Arrow} alt="" /></span>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label>Project owner</label>
+              <input
+                type="text"
+                placeholder="Enter the project owner"
+                value={projectOwner}
+                onChange={(e) => setProjectOwner(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Estimated budget</label>
+              <div className="budget-box">
+                <div className="currency">$</div>
+                <input
+                  type="number"
+                  placeholder="Enter amount"
+                  value={estimatedBudget}
+                  onChange={(e) => setEstimatedBudget(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label>Project complexity</label>
+              <input
+                type="text"
+                placeholder="Enter the project complexity"
+                value={projectComplexity}
+                onChange={(e) => setProjectComplexity(e.target.value)}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Estimated project start date</label>
+              <div className="date-box">
+                <DatePicker
+                  format="DD/MM/YYYY"
+                  onChange={(date, dateString) => setStartDate(dateString)}
+                  className="antd-date"
+                />
+              </div>
+            </div>
+            <div className="form-group">
+              <label>Estimated project end date</label>
+              <div className="date-box">
+                <DatePicker
+                  format="DD/MM/YYYY"
+                  onChange={(date, dateString) => setEndDate(dateString)}
+                  className="antd-date"
+                />
+              </div>
+            </div>
+
+          </div>
+
+          <div className="create-btn-section">
+            <div className="create-btn-container">
+              <div className="create-btn-tab">
+                <button className='create-btn-2' onClick={handleSubmit}>Submit</button>
+                <button className='create-btn-2' onClick={handleCancel}>Cancel</button>
+              </div>
             </div>
           </div>
 
         </div>
-
-        <div className="create-btn-section">
-          <div className="create-btn-container">
-            <div className="create-btn-tab">
-              <button className='create-btn-2' onClick={handleSubmit}>Submit</button>
-              <button className='create-btn-2' onClick={handleCancel}>Cancel</button>
-            </div>
-          </div>
-        </div>
-
       </div>
     </div>
   );
